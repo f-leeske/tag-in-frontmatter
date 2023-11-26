42180e1dfc9247c1a8d3e86bf571c5a58c4af95c
Fabian Leeske <fabian.leeske@gmail.com>
2023-11-26 12:51:52 +0100

    works only if tags exist in file

--- main.ts
+++ main.ts
@@ -1,134 +1,91 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Plugin, PluginSettingTab, Setting, SuggestModal, editorEditorField } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
-
-interface MyPluginSettings {
-	mySetting: string;
+/**
+ * TODO find the position to insert a new tag in the YAML frontmatter
+ * 
+ * @param editor the editor instance
+ * @returns the line number to insert (the character position is always 0)
+ */
+async function findTagsInFrontmatter(editor: Editor): Promise<number> {
+	return 1;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+/**
+	 * I have no idea how this works, copied from the fantastic obsidian-git plugin:
+	 * https://github.com/denolehov/obsidian-git
+	 * 
+	 * TODO: Add all tags as suggestions
+	 */
+export class UserInputModal extends SuggestModal<string> {
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+	resolve: (
+		value: string | undefined | PromiseLike<string>
+	) => void;
 
-	async onload() {
-		await this.loadSettings();
-
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
+	constructor(placeholder: string) {
 		super(app);
+		this.setPlaceholder(placeholder);
 	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
+	open(): Promise<string> {
+		super.open();
+		this.inputEl.dispatchEvent(new Event("input"));
+		return new Promise((resolve) => {
+			return this.resolve = resolve;
+		});
+	}
+
+	selectSuggestion(value: string, evt: MouseEvent | KeyboardEvent): void {
+		if (this.resolve) {
+			let res;
+			res = value;
+			this.resolve(res);
+		}
+		super.selectSuggestion(value, evt);
 	}
 
 	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
+		if (this.resolve) this.resolve(undefined);
 	}
+
+	getSuggestions(query: string): string[] {
+		return [query];
+	}
+
+	renderSuggestion(value: string, el: HTMLElement): void {
+		el.setText(value);
+	}
+
+	onChooseSuggestion(item: string, evt: MouseEvent | KeyboardEvent) { }
 }
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+export default class TagInFM extends Plugin {
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
+	async onload() {
+		console.log('start loading of taginfm');
+
+		this.addCommand({
+			id: 'add-tag-frontmatter',
+			name: 'Add tag to Frontmatter',
+			editorCallback: this.addTagToFrontmatter
+		});
+
+		console.log('loading done')
 	}
 
-	display(): void {
-		const {containerEl} = this;
+	onunload() {
+		console.log('unload taginfm');
+	}
 
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+	/**
+	 * the actual command callback that asks user input and adds the entered tag to the frontmatter
+	 * 
+	 */
+	async addTagToFrontmatter(editor: Editor, view: MarkdownView) {
+		editor.focus()
+		const nameModal = new UserInputModal("Name of the new tag",);
+		let insertPosition = await findTagsInFrontmatter(editor)
+		const userInput = await nameModal.open()
+		editor.replaceRange('  - ' + userInput + '\n', { ch: 0, line: 2 });
 	}
 }
